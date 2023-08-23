2b5bd3488cc84bf24481a928c06787552de3cee2
bopaki <btebalo@gmail.com>
2023-08-23 15:47:21 +0200

    fized icon sizes

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,155 @@
+"use client"
 import Image from 'next/image'
-
-export default function Home() {
+import Link from 'next/link'
+import { useMediaQuery } from 'react-responsive';
+import { 
+  IdCardIcon, 
+  HomeIcon,
+  BellIcon, 
+  BadgeIcon, 
+  BoxIcon, 
+  PlusCircledIcon,
+  DashboardIcon,
+  ChatBubbleIcon,
+  BarChartIcon
+} from '@radix-ui/react-icons';
+import logo from './logo/26logo.png'
+const Sidebar = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className='bg-gray-800 text-white w-1/12 min-h-screen py-5'>
+      <div className='flex mx-2'>
+      <Image
+      src={logo}
+      unoptimized
+      className='h-8 w-8 mr-8'
+      sizes='100vw'
+      style={{
+        width: '80%',
+        height: '80%'
+      }}
+      alt='Logo'
+      />
+      <h2 className='text-2xl font-bold text-center mb-6'>Base App</h2>
       </div>
-
+      <nav>
+        <ul className=''>
+          <li className='flex items-center justify-center hover:bg-gray-700 mb-7'>
+            <Link 
+            className=''
+            href="/">
+              <HomeIcon 
+                className='mr-2 w-8 h-8'
+              />
+            </Link>
+          </li>
+          <li className='flex items-center justify-center hover:bg-gray-700 mb-7'>
+            <Link 
+            className=''
+            href="/">
+              <DashboardIcon 
+                className='mr-2 w-8 h-8'
+              />
+            </Link>
+          </li>
+          <li className='flex items-center justify-center hover:bg-gray-700 mb-7'>
+            <Link 
+            className=''
+            href="/">
+              <HomeIcon 
+                className='mr-2 w-8 h-8'
+              />
+            </Link>
+          </li>
+          <li className='flex items-center justify-center hover:bg-gray-700 mb-7'>
+            <Link 
+            className=''
+            href="/">
+              <ChatBubbleIcon 
+                className='mr-2 w-8 h-8'
+              />
+            </Link>
+          </li>
+          <li className='flex items-center justify-center hover:bg-gray-700 mb-7'>
+            <Link 
+            className=''
+            href="/">
+              <BarChartIcon
+                className='mr-2 w-8 h-8'
+              />
+            </Link>
+          </li>
+          <li className='flex items-center justify-center hover:bg-gray-700 mb-7'>
+            <Link 
+            className=''
+            href="/">
+              <IdCardIcon
+                className='mr-2 w-8 h-8'
+              />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+const TopNav = () =>{
+  return (
+    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+    <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+      Get started by editing&nbsp;
+      <code className="font-mono font-bold">app/page.tsx</code>
+    </p>
+    <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+      <a
+        className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        By{' '}
+        <Image
+          src="/vercel.svg"
+          alt="Vercel Logo"
+          className="dark:invert"
+          width={100}
+          height={24}
+          priority
+        />
+      </a>
+    </div>
+  </div>
+  );
+}
+const BottomNav = () =>{
+  return (
+    <nav className='bg-gray-800 text-white fixed bottom-0 left-0 right-0 p-2 md:hidden'>
+      <ul className='flex justify-around'>
+        <li>
+          <Link 
+          className='hover:text-blue-500'
+          href="/"><HomeIcon/></Link>
+        </li>
+        <li>
+          <Link 
+          className='hover:text-blue-500'
+          href="/"><PlusCircledIcon/></Link>
+        </li>
+        <li>
+          <Link 
+          className='hover:text-blue-500'
+          href="/"><BellIcon/></Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+export default function Home() {
+  const isSmallScreen = useMediaQuery({ maxWidth: 768 }); //breakpoint
+  return (
+    <div className='grid grid-cols-2'>
+    {!isSmallScreen && <Sidebar/>}
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/*<TopNav/>*/}
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -109,5 +231,7 @@ export default function Home() {
         </a>
       </div>
     </main>
+    {isSmallScreen && <BottomNav/>}
+    </div>
   )
 }
